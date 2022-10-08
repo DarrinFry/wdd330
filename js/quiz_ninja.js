734acbef425af942e8926da9990872e0adff27a5
Darrin Fry <productionstagemanager@gmail.com>
2022-10-07 22:13:32 -0600

    Notes and quiz ninja work

--- js/quiz_ninja.js
+++ js/quiz_ninja.js
@@ -79,6 +79,8 @@ const view = {
     question: document.getElementById('question'),
     result: document.getElementById('result'),
     info: document.getElementById('info'),
+    response: document.querySelector('#response'),
+
     render(target,content,attributes) {
         for(const key in attributes) {
             target.setAttribute(key, attributes[key]);
@@ -90,22 +92,42 @@ const view = {
     },
     hide(element){
         element.style.display = 'none';
+    },
+    setup(){
+        this.show(this.question);
+        this.show(this.response);
+        this.show(this.result);
+        this.hide(this.start);
+        this.render(this.score,game.score);
+        this.render(this.result,'');
+        this.render(this.info,'');
+        this.resetForm();
+    },
+    resetForm(){
+        this.response.answer.value = '';
+        this.response.answer.focus();
+    },
+    teardown(){
+        this.hide(this.question);
+        this.hide(this.response);
+        this.show(this.start);
     }
+    
 };
 
 const game = {
-    start(quiz){
-        this.questions = [...quiz];
-        this.score = 0;
-        // main game loop
-        for(const question of this.questions){
-        this.question = question;
-        this.ask();
-        }
-        // end of main game loop
-        this.gameOver();
-        view.hide(view.start);
-    },
+    // start(quiz){
+    //     this.questions = [...quiz];
+    //     this.score = 0;
+    //     this.ask();
+    //     main game loop
+    //     for(const question of this.questions){
+    //     this.question = question;
+    //     }
+    //     // end of main game loop
+    //     this.gameOver();
+    //     view.hide(view.start);
+    // },
     // ask(){
     //     const question = `What is ${this.question.name}'s real name?`;
     //     const response =  prompt(question);
@@ -123,29 +145,55 @@ const game = {
     // gameOver(){
     //     alert(`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
     // }
-    ask(){
-        const question = `What is ${this.question.name}'s real name?`;
-        view.render(view.question,question);
-        const response =  prompt(question);
-        this.check(response);
+    start(quiz){
+        this.score = 0;
+        this.questions = [...quiz];
+        view.setup();
+        this.ask();
     },
-    check(response){
+    ask(name){
+        if(this.questions.length > 0) {
+            this.question = this.questions.pop();
+            const question = `What is ${this.question.name}'s real name?`;
+            view.render(view.question,question);
+        }
+        else {
+            this.gameOver();
+        }
+    },
+    // check(response){
+    //     const answer = this.question.realName;
+    //     if(response === answer){
+    //     view.render(view.result,'Correct!',{'class':'correct'});
+    //     alert('Correct!');
+    //     this.score++;
+    //     view.render(view.score,this.score);
+    //     } else {
+    //     view.render(view.result,`Wrong! The correct answer was ${answer}`,{'class':'wrong'});
+    //     alert(`Wrong! The correct answer was ${answer}`);
+    //     }
+    // },
+    check(event){
+        event.preventDefault();
+        const response = view.response.answer.value;
         const answer = this.question.realName;
         if(response === answer){
-        view.render(view.result,'Correct!',{'class':'correct'});
-        alert('Correct!');
-        this.score++;
-        view.render(view.score,this.score);
+            view.render(view.result,'Correct!',{'class':'correct'});
+            this.score++;
+            view.render(view.score,this.score);
         } else {
-        view.render(view.result,`Wrong! The correct answer was ${answer}`,{'class':'wrong'});
-        alert(`Wrong! The correct answer was ${answer}`);
+            view.render(view.result,`Wrong! The correct answer was ${answer}`,{'class':'wrong'});
         }
+        view.resetForm();
+        this.ask();
     },
     gameOver(){
         view.render(view.info,`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
-        view.show(view.start);
+        view.teardown();
     }
 }
 
 view.start.addEventListener('click', () => game.start(quiz), false);
 //game.start(quiz);
+view.response.addEventListener('submit', (event) => game.check(event), false);
+view.hide(view.response);
--- js/week4_coding_practice.js
+++ js/week4_coding_practice.js
@@ -1,27 +1,28 @@
-const form = document.forms['search'];
-const input = form.elements.searchInput;
+// const form = document.forms['search'];
+// const input = form.elements.searchInput;
 
 // input.addEventListener('focus', () => alert('focused'), false);
 // input.addEventListener('blur', () => alert('blurred'), false);
 // input.addEventListener('change', () => alert('changed'), false);
 
-input.value = 'Search Here';
-form.addEventListener ('submit', search, false);
+// input.value = 'Search Here';
+// form.addEventListener ('submit', search, false);
 
-function search(event) {
-    alert(`You Searched for: ${input.value}`);
-    event.preventDefault();
-}
-input.addEventListener('focus', function(){
-    if (input.value==='Search Here') {
-        input.value = '' 
-    }
-}, false);
+// function search(event) {
+//     alert(`You Searched for: ${input.value}`);
+//     event.preventDefault();
+// }
+// input.addEventListener('focus', function(){
+//     if (input.value==='Search Here') {
+//         input.value = '' 
+//     }
+// }, false);
 
-input.addEventListener('blur', function(){
-    if(input.value === '') {
-        input.value = 'Search Here';
-} }, false);
+// input.addEventListener('blur', function(){
+//     if(input.value === '') {
+//         input.value = 'Search Here';
+// } }, false);
+
 
 const heroForm = document.forms['hero'];
 heroForm.addEventListener('submit', makeHero, false);
@@ -41,7 +42,35 @@ function makeHero(event) {
     //     }
     // };
     hero.powers = [...heroForm.powers].filter(box => box.checked).map(box => box.value);
+    hero.category = heroForm.category.value;
+    hero.age = heroForm.age.value;
+    hero.city = heroForm.city.value;
+    hero.origin = heroForm.origin.value;
 
     alert(JSON.stringify(hero)); // convert object to JSON string and display in alert dialog
     return hero;
-}
+};
+heroForm.addEventListener('submit',validate,false);
+
+function validate(event) {
+    const firstLetter = heroForm.heroName.value[0];
+    if (firstLetter.toUpperCase() === 'X') {
+        event.preventDefault();
+        alert('Your name is not allowed to start with X!');
+    }
+};
+const label = heroForm.querySelector('label');
+const error = document.createElement('div');
+error.classList.add('error');
+error.textContent = '! Your name is not allowed to start with X.';
+label.append(error);
+
+function validateInline() {
+    const heroName = this.value.toUpperCase();
+    if(heroName.startsWith('X')){
+    error.style.display = 'block';
+    } else {
+    error.style.display = 'none';
+    }
+};
+
