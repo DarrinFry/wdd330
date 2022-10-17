289acb4f94b67fe2b2d5775764e5ba4f4dff32fd
Darrin Fry <productionstagemanager@gmail.com>
2022-10-16 20:03:33 -0600

    ToDo Framework

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const links = [
     {label: "Week 3 notes",url: "week3/index.html"},
     {label: "Week 4 notes",url: "week4/index.html"},
     {label: "Week 5 notes",url: "week5/index.html"},
-    // {label: "Week 6 notes",url: "week6/index.html"},
+    {label: "Week 6 notes",url: "week6/index.html"},
     // {label: "Week 7 notes",url: "week7/index.html"},
     // {label: "Week 8 notes",url: "week8/index.html"},
     // {label: "Week 9 notes",url: "week9/index.html"},
