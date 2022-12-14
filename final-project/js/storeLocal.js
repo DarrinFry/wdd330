5a2c8d9dafee01bcf433f221bc00a61df9571694
Darrin Fry <productionstagemanager@gmail.com>
2022-12-14 00:40:00 -0700

    Adjustments to math, style, and load last customer

--- final-project/js/storeLocal.js
+++ final-project/js/storeLocal.js
@@ -14,12 +14,35 @@ export function mySave() {
     
     var myContent = `Customer: ${customerName} \nVolume: ${volumeNumber}  \nDiamond Melee CTW: ${diamondMeleeCTW}  \nStone Count: ${stoneCount}  \nOther Stones: ${otherStones}  \nOther Labor: ${otherLabor}`;
     localStorage.setItem("myContent", myContent);
+    localStorage.setItem("volumeNumber", volumeNumber);
+    localStorage.setItem("diamondMeleeCTW", diamondMeleeCTW);
+    localStorage.setItem("stoneCount", stoneCount);
+    localStorage.setItem("otherStones", otherStones);
+    localStorage.setItem("otherLabor", otherLabor);
+    localStorage.setItem("customerName", customerName);
+
     console.log(myContent);
 }
 
 
+
 export function myLoad() {
     console.log("myload called");
-var myContent = localStorage.getItem("myContent");
-document.getElementById("loadOutputDiv").innerHTML = myContent;
+    var volumeNumber = parseFloat(localStorage.getItem("volumeNumber"));
+    var diamondMeleeCTW = parseFloat(localStorage.getItem("diamondMeleeCTW"));
+    var stoneCount = parseFloat(localStorage.getItem("stoneCount"));
+    var otherStones = parseFloat(localStorage.getItem("otherStones"));
+    var otherLabor = parseFloat(localStorage.getItem("otherLabor"));
+    var customerName = localStorage.getItem("customerName");
+    var myContent = localStorage.getItem("myContent");
+
+    document.getElementById("loadOutputDiv").innerHTML = myContent;
+
+    document.getElementById("volumeNumber").value = volumeNumber;
+    document.getElementById("diamondMeleeCTW").value = diamondMeleeCTW;
+    document.getElementById("stoneCount").value = stoneCount;
+    document.getElementById("otherStones").value = otherStones;
+    document.getElementById("otherLabor").value = otherLabor;
+    document.getElementById("customerName").value = customerName;
+    console.log (stoneCount)
 }
